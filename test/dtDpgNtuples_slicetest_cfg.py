d46b279884e802e710c2829475d64bbc4fdf3d4a
Carlo Battilana <carlo.battilana@gmail.com>
2019-07-20 18:53:25 +0200

    Slice test cfg in line with analysis package"

--- test/dtDpgNtuples_slicetest_cfg.py
+++ test/dtDpgNtuples_slicetest_cfg.py
@@ -4,11 +4,12 @@ from Configuration.StandardSequences.Eras import eras
 
 import subprocess
 import sys
+import os
 
 options = VarParsing.VarParsing()
 
 options.register('globalTag',
-                 '106X_dataRun2_v10', #default value
+                 '106X_dataRun3_Express_v2', #default value
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  "Global Tag")
@@ -19,17 +20,29 @@ options.register('nEvents',
                  VarParsing.VarParsing.varType.int,
                  "Maximum number of processed events")
 
+options.register('runNumber',
+                 '329806', #default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.int,
+                 "Run number to be looked for in either 'inputFolderCentral' or 'inputFolderDT' folders")
+
 options.register('inputFile',
-                 '/eos/cms/store/group/dpg_dt/comm_dt/commissioning_2019_data/root/run329614_streamDQM_fu-c2f13-09-03.root', #default value
+                 '', #default value
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
-                 "The input file to be processed")
+                 "The input file to be processed, if non null overrides runNumber based input file selection")
 
-options.register('inputFolder',
-                 '', #default value
+options.register('inputFolderCentral',
+                 '/eos/cms/store/data/Commissioning2019/MiniDaq/RAW/v1/', #default value
                   VarParsing.VarParsing.multiplicity.singleton,
                   VarParsing.VarParsing.varType.string,
-                 "EOS folder with input files")
+                 "Base EOS folder with input files from MiniDAQ runs with central tier0 transfer")
+
+options.register('inputFolderDT',
+                 '/eos/cms/store/group/dpg_dt/comm_dt/commissioning_2019_data/root/', #default value
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                 "Base EOS folder with input files from MiniDAQ runs with DT 'private' tier0 transfer")
 
 options.register('tTrigFile',
                  '', #default value
@@ -37,11 +50,23 @@ options.register('tTrigFile',
                  VarParsing.VarParsing.varType.string,
                  "File with customised DT tTrigs, used only if non ''")
 
-options.register('ntupleName',
-                 './DTDPGNtuple_10_6_0_SX5.root', #default value
+options.register('t0File',
+                 '', #default value
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
-                 "Folder and name ame for output ntuple")
+                 "File with customised DT t0is, used only if non ''")
+
+options.register('vDriftFile',
+                 '', #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "File with customised DT vDrifts, used only if non ''")
+
+options.register('ntupleName',
+                 '', #default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Folder and name ame for output ntuple, if non null overrides 'standard' naming based on runNumber option")
 
 
 options.parseArguments()
@@ -59,38 +84,83 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 
 process.GlobalTag.globaltag = cms.string(options.globalTag)
 
-if options.tTrigFile != '' :
+if options.tTrigFile  != '' or \
+   options.vDriftFile != '' or \
+   options.t0File != '' :
+    process.GlobalTag.toGet = cms.VPSet()
 
-    process.GlobalTag.toGet = cms.VPSet(
-        cms.PSet(record = cms.string("DTTtrigRcd"),
-                 tag = cms.string("ttrig"),
-                 connect = cms.string("sqlite_file:" + options.tTrigFile),
-                 label = cms.untracked.string("cosmics")
-                 )
-        )
+if options.tTrigFile != '' :
+    process.GlobalTag.toGet.append(cms.PSet(record = cms.string("DTTtrigRcd"),
+                                            tag = cms.string("ttrig"),
+                                            connect = cms.string("sqlite_file:" + options.tTrigFile),
+                                            label = cms.untracked.string("cosmics")
+                                        )
+                               )
+
+if options.vDriftFile != '' :
+    process.GlobalTag.toGet.append(cms.PSet(record = cms.string("DTMtimeRcd"),
+                                            tag = cms.string("vDrift"),
+                                            connect = cms.string("sqlite_file:" + options.vDriftFile)
+                                        )
+                                   )
+
+if options.t0File != '' :
+    process.GlobalTag.toGet.append(cms.PSet(record = cms.string("DTT0Rcd"),
+                                            tag = cms.string("t0"),
+                                            connect = cms.string("sqlite_file:" + options.t0File)
+                                        )
+                                   )
+    
 
 process.source = cms.Source("PoolSource",
                             
-        fileNames = cms.untracked.vstring(""),
+        fileNames = cms.untracked.vstring(),
         secondaryFileNames = cms.untracked.vstring()
 
 )
 
-if (options.inputFile == '' and options.inputFolder == '') or \
-   (options.inputFile != '' and options.inputFolder != '') :
+if options.inputFile != '' :
 
-    print "[dtDpgNtuples_slicetest_cfg.py]: inputFile and inputFolder can be non-null only one at a time. quitting."
-    sys.exit(999)
-    
-if options.inputFile != "" :
+    print "[dtDpgNtuples_slicetest_cfg.py]: inputFile parameter is non-null running on file:\n\t\t\t" + options.inputFile
     process.source.fileNames = cms.untracked.vstring("file://" + options.inputFile)
 
-if options.inputFolder != "" :
-    files = subprocess.check_output(["ls", options.inputFolder])
-    process.source.fileNames = ["file://" + options.inputFolder + "/" + f for f in files.split()]
+else :
+
+    runStr = str(options.runNumber).zfill(9)
+    runFolder = options.inputFolderCentral + "/" + runStr[0:3] + "/" + runStr[3:6] + "/" + runStr[6:] + "/00000"
+
+    print "[dtDpgNtuples_slicetest_cfg.py]: looking for files under:\n\t\t\t" + runFolder
+    
+    if os.path.exists(runFolder) :
+        files = subprocess.check_output(["ls", runFolder])
+        process.source.fileNames = ["file://" + runFolder + "/" + f for f in files.split()]
+
+    else :
+        print "[dtDpgNtuples_slicetest_cfg.py]: files not found there, looking under:\n\t\t\t" + options.inputFolderDT
+
+        files = subprocess.check_output(["ls", options.inputFolderDT])
+        filesFromRun = []
+
+        for f in files.split() :
+            if f.find(runStr[3:]) > -1 :
+                filesFromRun.append(f)
+
+        if len(filesFromRun) == 1 :
+            process.source.fileNames.append("file://" + options.inputFolderDT + "/" + filesFromRun[0])
+
+        else :
+            print "[dtDpgNtuples_slicetest_cfg.py]: " + str(len(filesFromRun)) + " files found, can't run!"
+            sys.exit(999)
+
+print process.source.fileNames
+
+if options.ntupleName == '' :
+    ntupleName = "./DTDPGNtuple_run" + str(options.runNumber) + ".root"
+else :
+    ntupleName = options.ntupleName
 
 process.TFileService = cms.Service('TFileService',
-        fileName = cms.string(options.ntupleName)
+        fileName = cms.string(ntupleName)
     )
 
 process.load('Configuration/StandardSequences/GeometryRecoDB_cff')
@@ -98,6 +168,8 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
 process.load('EventFilter.DTRawToDigi.dtab7unpacker_cfi')
+
+process.dtAB7unpacker.channelMapping = cms.untracked.string("july2019")
 
 process.load('RecoLocalMuon.Configuration.RecoLocalMuonCosmics_cff')
 
