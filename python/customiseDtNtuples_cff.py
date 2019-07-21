7d1631534ec52d1a3a55689d442229e5f08c2dd2
Carlo Battilana <carlo.battilana@gmail.com>
2019-07-21 11:36:03 +0200

    For tutorial, few changes in customization for MC

--- python/customiseDtNtuples_cff.py
+++ python/customiseDtNtuples_cff.py
@@ -7,13 +7,13 @@ def customiseForRunningOnMC(process, pathName) :
         print "[customiseForRunningOnMC]: updating ntuple input tags"
 
         process.dtNtupleProducer.genPartTag = "prunedGenParticles"
-        process.dtNtupleProducer.puInfoTag = "addPileupInfo"
+        # process.dtNtupleProducer.puInfoTag = "addPileupInfo"
 
         process.dtNtupleProducer.lumiScalerTag = "none"
 
-        process.dtNtupleProducer.ph1TwinMuxInTag = "none"
-        process.dtNtupleProducer.ph1TwinMuxOutTag = "none"
-        process.dtNtupleProducer.ph1TwinMuxInThTag = "none"
+        # process.dtNtupleProducer.ph1TwinMuxInTag = "none"
+        # process.dtNtupleProducer.ph1TwinMuxOutTag = "none"
+        # process.dtNtupleProducer.ph1TwinMuxInThTag = "none"
 
         if hasattr(process,pathName) :
             print "[customiseForRunningOnMC]: adding prunedGenParitcles"
