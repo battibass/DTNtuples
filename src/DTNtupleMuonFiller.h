48ba6afbc2332b60abb07a49dac50e1373913319
Leonardo Lunerti <leonardo.lunerti2@unibo.it>
2020-03-23 17:34:21 +0100

    Comments added

--- src/DTNtupleMuonFiller.h
+++ src/DTNtupleMuonFiller.h
@@ -55,8 +55,8 @@ class DTNtupleMuonFiller : public DTNtupleBaseFiller
 
   short   m_mu_nMuons; 
 
-  std::vector<float> m_mu_pt;     // muon pT (float)
-  std::vector<float> m_mu_phi;    // muon phi (float in [-pi:pi] range) 
+  std::vector<float> m_mu_pt;     // muon pT 
+  std::vector<float> m_mu_phi;    // muon phi 
   std::vector<float> m_mu_eta;    // muon eta
   std::vector<short> m_mu_charge; // muon charge
 
@@ -65,37 +65,38 @@ class DTNtupleMuonFiller : public DTNtupleBaseFiller
   std::vector<bool>  m_mu_isTracker;
   std::vector<bool>  m_mu_isRPC;
 
-  std::vector<bool>  m_mu_isLoose;//Loose muon ID
+  std::vector<bool>  m_mu_isLoose; //Loose muon ID
   std::vector<bool>  m_mu_isMedium;//Medium muon ID
-  std::vector<bool>  m_mu_isTight;//Tight muon ID
+  std::vector<bool>  m_mu_isTight; //Tight muon ID
 
   std::vector<float> m_mu_trkIso03;//Relative tracker isolation (0.3 cone)
-  std::vector<float> m_mu_PFIso04;//Relative PF-isolation (delta beta corrected, 0.4 cone)
+  std::vector<float> m_mu_PFIso04; //Relative PF-isolation (delta beta corrected, 0.4 cone)
   
   std::vector<float> m_mu_trk_dxy;//Inner track dxy parameter with respect to the primary vertex
-  std::vector<float> m_mu_trk_dz;//Inner track dz parameter with respect to the primary vertex
+  std::vector<float> m_mu_trk_dz; //Inner track dz parameter with respect to the primary vertex
 
   std::vector<int>   m_mu_trk_numberOfPixelHits;//Number of valid pixel hits
   std::vector<int>   m_mu_trk_numberOfLayerHits;//Number of valid layer hits
   
   std::vector<int>   m_mu_TRKMu_numberOfMatchedStations;//Number of matched stations
   std::vector<int>   m_mu_STAMu_numberOfValidMuonHits;//Number of valid muon hits
-  std::vector<float> m_mu_STAMu_normChi2;// chi2/ndof
-  std::vector<float> m_mu_GLBMu_normChi2;// chi2/ndof
+  std::vector<float> m_mu_STAMu_normChi2;             // chi2/ndof
+  std::vector<float> m_mu_GLBMu_normChi2;             // chi2/ndof
 
-  std::vector<int>   m_mu_nMatches;
+  std::vector<int>   m_mu_nMatches;// Number of DT matches
 
-  TClonesArray *m_mu_matches_wh;  
-  TClonesArray *m_mu_matches_sec;
-  TClonesArray *m_mu_matches_st;
-  TClonesArray *m_mu_matches_x;
-  TClonesArray *m_mu_matches_y;
-  TClonesArray *m_mu_matches_phi;
-  TClonesArray *m_mu_matches_eta;
-  TClonesArray *m_mu_matches_edgeX;
-  TClonesArray *m_mu_matches_edgeY;
+  TClonesArray *m_mu_matches_wh; //Index associated to the matched chamber wheel 
+  TClonesArray *m_mu_matches_sec;//Index associated to the matched chamber sector
+  TClonesArray *m_mu_matches_st; //Index associated to the matched chamber station
+  TClonesArray *m_mu_matches_x;//X position of the track on the matched chamber
+  TClonesArray *m_mu_matches_y;//Y position of the track on the matched chamber
 
-  TClonesArray *m_mu_matchSegIdx;// Matching segment index
+  TClonesArray *m_mu_matches_phi;//Phi of the (x,y) position on the matched chamber (global reference frame)
+  TClonesArray *m_mu_matches_eta;//Eta of the (x,y) position on the matched chamber (global reference frame)
+  TClonesArray *m_mu_matches_edgeX;// distance to closest edge in X on the matched chamber
+  TClonesArray *m_mu_matches_edgeY;// distance to closest edge in Y on the matched chamber
+
+  TClonesArray *m_mu_matchSegIdx;// Index associated to the matching segment
 
   float computeTrkIso (const reco::MuonIsolation& isolation, float muonPt);
   float computePFIso  (const reco::MuonPFIsolation& PFisolation, float muonPt);
